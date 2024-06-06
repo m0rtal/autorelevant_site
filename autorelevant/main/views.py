0f75bdb4dcbbd1cf99a96ed11ffe0255ff5bb469
jollyreap <leonid1988mail@mail.ru>
2024-06-06 16:53:04 +0300

    локальное сохранение файла

--- autorelevant/main/views.py
+++ autorelevant/main/views.py
@@ -45,7 +45,7 @@ async def upload(request):
             filename = fs.save(uploaded_file.name, uploaded_file)
             uploaded_file_url = fs.url(filename)
 
-            df = pd.read_excel('/home/jollyreap/ML/autorelevant_site/autorelevant' + uploaded_file_url)
+            df = pd.read_excel(uploaded_file_url)
 
             df = df.rename({
                 'Запрос': 'search_string',
@@ -54,10 +54,11 @@ async def upload(request):
 
             df.loc[:, 'region'] = int(selected_value1)
             df.loc[:, 'location'] = selected_value2
-            df = df[['url', 'search_string', 'region', 'location']]
+            df = df[['ID', 'url', 'search_string', 'region', 'location']]
 
             for i in range(len(df)):
                 req = df[df.index == i]
+                req = req.drop(['ID'], axis=1)
                 ya_req = req.loc[:, req.columns != 'location']
                 google_req = req.loc[:, req.columns != 'region']
                 ya_req = dict(zip(ya_req.columns, ya_req.values[0]))
@@ -70,15 +71,37 @@ async def upload(request):
                 # yandex
                 async with aiohttp.ClientSession() as session:
                     async with session.get(url='http://0.0.0.0:5000/process-url/', params=ya_req) as response:
-                        ya_response = await response.json(encoding='utf-8')
+                        ya_response = json.loads(await response.json(encoding='utf-8'))
 
                 # google
                 async with aiohttp.ClientSession() as session:
                     async with session.get(url='http://0.0.0.0:5000/search-google/', params=google_req) as response:
-                        google_response = await response.json(encoding='utf-8')
+                        google_response = json.loads(await response.json(encoding='utf-8'))
 
-            print(ya_response)
-            print(google_response)
+                ya_lsi = pd.DataFrame.from_dict(ya_response['lsi'])
+                ya_increase_qty = pd.DataFrame(ya_response['увеличить частотность'].values(), index=ya_response['увеличить частотность'].keys(), columns=['увеличить частотность yandex'])
+                ya_decrease_qty = pd.DataFrame(ya_response['уменьшить частотность'].values(), index=ya_response['уменьшить частотность'].keys(), columns=['уменьшить частотность yandex'])
+                ya_urls = ya_response['обработанные ссылки']
+
+                google_lsi = pd.DataFrame.from_dict(google_response['lsi'])
+                google_increase_qty = pd.DataFrame(google_response['увеличить частотность'].values(), index=google_response['увеличить частотность'].keys(), columns=['увеличить частотность google'])
+                google_decrease_qty = pd.DataFrame(google_response['уменьшить частотность'].values(), index=google_response['уменьшить частотность'].keys(), columns=['уменьшить частотность google'])
+                google_urls = google_response['обработанные ссылки']
+
+                lsi = pd.concat([ya_lsi, google_lsi], axis=0).to_dict()[0]
+                increase_qty = pd.merge(ya_increase_qty, google_increase_qty, left_index=True, right_index=True, how='outer').fillna(0, axis=0)
+                decrease_qty = pd.merge(ya_decrease_qty, google_decrease_qty, left_index=True, right_index=True, how='outer').fillna(0, axis=0)
+
+                df.loc[i, 'lsi'] = [lsi]
+                df.loc[i, 'увеличить частотность yandex'] = [increase_qty.loc[:, 'увеличить частотность yandex'].to_dict()]
+                df.loc[i, 'увеличить частотность google'] = [increase_qty.loc[:, 'увеличить частотность google'].to_dict()]
+                df.loc[i, 'уменьшить частотность yandex'] = [decrease_qty.loc[:, 'уменьшить частотность yandex'].to_dict()]
+                df.loc[i, 'уменьшить частотность google'] = [decrease_qty.loc[:, 'уменьшить частотность google'].to_dict()]
+                df.loc[i, 'Yandex выдача'] = [ya_urls]
+                df.loc[i, 'Goole выдача'] = [google_urls]
+
+            with pd.ExcelWriter('autorelevant_site/test.xlsx') as writer:
+                df.to_excel(writer, sheet_name='test', index=False)
 
             return HttpResponse(
                 f'File uploaded successfully: <a href="{uploaded_file_url}">{uploaded_file_url}</a><br>'
